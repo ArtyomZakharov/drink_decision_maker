57d8c7657a45556fc768c3ae9a413acc4a0588a3
Artyom <artyom.zakharov@outlook.com>
2016-06-13 10:33:44 +0600

    lil fix

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 exports.makeADecision = function() {
-    console.log(Math.ceil(Math.random() * 10) % 2 == 0 ? "Nah, fam" : "Go have a drink");
+    return (Math.ceil(Math.random() * 10) % 2 == 0 ? "Nah, fam" : "Go have a drink");
 }
